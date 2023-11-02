4c829f6f658be8f75f2c86baaeebb0b5da303732
NBDatsuya <2646809050@qq.com>
2023-11-03 02:16:39 +0800

    init commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <view class="content">
+    <view class="text-area">
+      <text class="title">启动成功</text>
+    </view>
+  </view>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+</style>
