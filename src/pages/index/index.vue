f471c4470ca1ec2f85e459d0227a43796c37189b
NBDatsuya <2646809050@qq.com>
2023-11-03 15:41:43 +0800

    feat: tabbar

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,6 +2,7 @@
   <view class="content">
     <view class="text-area">
       <text class="title">启动成功</text>
+      <text class="title" color="$uni-color-primary">欢迎使用信东智能投顾</text>
     </view>
   </view>
 </template>
