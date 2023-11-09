632e8f4950262b4ca358008d4d6e3856aebfbc0f
NBDatsuya <2646809050@qq.com>
2023-11-09 17:36:15 +0800

    refactor: using api (skeleton)

--- src/pages/quesnaire/index.vue
+++ src/pages/quesnaire/index.vue
@@ -1,116 +1,53 @@
 <template>
-  <view class="questionnaire">
-    <view class="question"
-          v-for="(question, index) in questions"
-          :key="index">
-      <p class="question-text">{{ question.text }}</p>
-      <view class="options">
-        <radio-group>
-          <label
-              class="option"
-              v-for="(option, optionIndex) in question.options"
-              :key="optionIndex"
-          >
-            <radio
-                :id="`question_${index}_option_${optionIndex}`"
-                :value="option.value"
-                v-model="questionResponses[index]"></radio>
-            <!--
-                @change="handleCheckboxChange(index, option.value)"-->
-            <span>{{ option.value }}. {{ option.label }}</span></label>
-        </radio-group>
-      </view>
+    <view class="questionnaire">
+        <view class="question"
+              v-for="(question, index) in questions"
+              :key="index">
+            <p class="question-text">{{ question.text }}</p>
+            <view class="options">
+                <radio-group>
+                    <label
+                            class="option"
+                            v-for="(option, optionIndex) in question.options"
+                            :key="optionIndex"
+                    >
+                        <radio
+                                :id="`question_${index}_option_${optionIndex}`"
+                                :value="option.value"
+                                v-model="questionResponses[index]"></radio>
+                        <!--
+                            @change="handleCheckboxChange(index, option.value)"-->
+                        <span>{{ option.value }}. {{ option.label }}</span></label>
+                </radio-group>
+            </view>
+        </view>
+        <button type="submit">提交</button>
     </view>
-    <button
-        type="submit"
-        @click="navigateToRecommendationPage"
-    >
-      提交
-    </button>
-  </view>
 </template>
 
 <script setup lang="ts">
 import {computed, ref} from 'vue';
 
-const questions = [
-  {
-    text: "1. 您的年龄是？",
-    options: [
-      {label: "18-25岁", value: "A"},
-      {label: "26-35岁", value: "B"},
-      {label: "36-45岁", value: "C"},
-      {label: "46-55岁", value: "D"},
-      {label: "56岁以上", value: "E"},
-    ],
-  },
-  {
-    text: "2. 您当前的职业状况是？",
-    options: [
-      {label: "学生", value: "A"},
-      {label: "工作不稳定或暂无工作", value: "B"},
-      {label: "稳定工作，固定收入", value: "C"},
-      {label: "自主创业或自由职业", value: "D"},
-      {label: "已退休", value: "E"},
-    ],
-  },
-  {
-    text: "3. 您的投资经验是？",
-    options: [
-      {label: "无经验", value: "A"},
-      {label: "1-3年", value: "B"},
-      {label: "4-6年", value: "C"},
-      {label: "7年以上", value: "D"},
-    ],
-  },
-  {
-    text: "4. 您预计的投资期限是？",
-    options: [
-      {label: "3个月以下", value: "A"},
-      {label: "3-12个月", value: "B"},
-      {label: "1-3年", value: "C"},
-      {label: "3-5年", value: "D"},
-      {label: "5年以上", value: "E"},
-    ],
-  },
-  {
-    text: "5. 当您的投资面临亏损时，您的反应是？",
-    options: [
-      {label: "马上卖出，不能接受任何亏损", value: "A"},
-      {label: "观望，看是否有恢复的迹象", value: "B"},
-      {label: "买入更多，看到了投资机会", value: "C"},
-      {label: "不会做出过多反应，坚信长期投资策略", value: "D"},
-    ],
-  },
-  {
-    text: "6. 您期望的年回报率是？",
-    options: [
-      {label: "1-3%", value: "A"},
-      {label: "4-6%", value: "B"},
-      {label: "7-10%", value: "C"},
-      {label: "10%以上", value: "D"},
-    ],
-  },
-  {
-    text: "7. 您的财务状况如何？",
-    options: [
-      {label: "无储蓄，或有负债", value: "A"},
-      {label: "有一定储蓄，但无其他投资", value: "B"},
-      {label: "有多元化的投资组合", value: "C"},
-      {label: "财务宽裕，有大量的可投资资金", value: "D"},
-    ],
-  },
-  // 添加更多问题
-];
+const questions = ref([
+    {
+    "title": {
+      "id": 0,
+      "title": "这是问题"
+        },
+    "options": [
+            {
+                "id": 0,
+                "question": 0,
+                "title": "这是选项",
+                "value": 0,
+                "questionType": 0
+      }
+    ]
+  }
+]);
 
-const questionResponses = ref(Array(questions.length).fill(''));
+const questionResponses = ref(Array(questions.value.length).fill(''));
 const recommendation = ref('');
-const navigateToRecommendationPage = () => {
-  // 执行页面导航
-  uni.switchTab({
-    url: "../../pages/recommend/index"
-  });
-};
 </script>
 
 
